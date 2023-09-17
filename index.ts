f4bd908884de005a0c13986dea967d601b82ccb7
Faisal <saleemfs27@gmail.com>
2023-09-17 20:38:41 +0200

    Init

---
+++ index.ts
@@ -0,0 +1,54 @@
+import { v4 as uuidv4 } from "uuid";
+
+const server = Bun.serve<{ userId: string }>({
+  port: 3000,
+  websocket: {
+    open: (ws) => {
+      ws.subscribe("chat");
+      console.log("Client connected");
+      const userId = ws.data.userId;
+
+      const payload = JSON.stringify({
+        message: `Guest ${userId} has entered the chat`,
+      });
+      server.publish("chat", payload);
+    },
+    message: (ws, message) => {
+      console.log("Client sent message", message);
+      const userId = ws.data.userId;
+
+      const payload = JSON.stringify({
+        message: `Guest ${userId}: ${message}`,
+      });
+      server.publish("chat", payload);
+    },
+    close: (ws) => {
+      console.log("Client disconnected");
+      ws.unsubscribe("chat");
+      const userId = ws.data.userId;
+
+      const payload = JSON.stringify({
+        message: `Guest ${userId} has left the chat`,
+      });
+      server.publish("chat", payload);
+    },
+  },
+  fetch(req, server) {
+    const url = new URL(req.url);
+
+    // upgrade request to websocket
+    const userId = uuidv4();
+    if (url.pathname === "/chat") {
+      const upgraded = server.upgrade(req, {
+        data: { userId },
+      });
+      if (!upgraded) {
+        return new Response("Upgrade failed", { status: 400 });
+      }
+    }
+
+    return new Response("Test");
+  },
+});
+
+console.log(`Listening on port ${server.port}`);
